3c6ed7e1bbcde086be44cb73c710b5834c6f90de
nasubiman <nasubimanza@gmail.com>
2023-09-14 15:39:12 +0900

    とりあえず一息

--- include/siritori.py
+++ include/siritori.py
@@ -1,10 +1,14 @@
 from include import siritori_library
 def siritori(text , flag):
     if(not flag):
+        siritori_library.add_used_word(text)
         return 'しりとりを始めるよ！ 終わりたかったら!owari と打ってね  まずは自分から\n しりとりますたー', True
 
     elif(text == '!owari'):
         return "しりとりを終了するよ！ またね！\n あなたのスコアは無しです", False
     
+    elif(text == "!showlist"):
+        return siritori_library.show_used_word, True
+    
     elif True:
-        return siritori_library.hoe(text[0] , text[-1]) , True
+        return siritori_library.return_word(text) , True
--- include/siritori_library.py
+++ include/siritori_library.py
@@ -1,47 +1,28 @@
-class ClassName:
-    # クラス変数（クラス全体で共有される変数）
-    class_variable = "This is a class variable"
-
-    # イニシャライザ（クラスのインスタンスを初期化するメソッド）
-    def __init__(self, param1, param2):
-        # インスタンス変数（各インスタンスに固有の変数）
-        self.param1 = param1
-        self.param2 = param2
-
-    # インスタンスメソッド（インスタンスに対して操作を行うメソッド）
-    def instance_method(self):
-        return f"This is an instance method with param1={self.param1} and param2={self.param2}"
-
-    # クラスメソッド（クラス全体に対して操作を行うメソッド）
-    @classmethod
-    def class_method(cls):
-        return f"This is a class method with class_variable={cls.class_variable}"
-
-    # スタティックメソッド（特定のクラスまたはインスタンスに依存しないメソッド）
-    @staticmethod
-    def static_method():
-        return "This is a static method"
-
-# クラスのインスタンスを作成
-obj = ClassName("Value1", "Value2")
-
-# インスタンスメソッドを呼び出し
-result1 = obj.instance_method()
-print(result1)
-
-# クラスメソッドを呼び出し
-result2 = ClassName.class_method()
-print(result2)
-
-# スタティックメソッドを呼び出し
-result3 = ClassName.static_method()
-print(result3)
+used_word_list = []
 
 word_list = ["apple", "elephant", "tiger", "rabbit", "cat", "dog"]
 
 current_word = None
-player_score = 0
 
-def hoe(first , last):
-    print(text + "is last word")
-    return text + "するますたー"
+def add_used_word(word):
+    used_word_list.append(word)
+
+def show_used_word():
+    print(used_word_list)
+    return used_word_list
+
+def check_first_word(word):
+    if(word[0] == "た" or word[0] == "タ"):
+        return True
+    else:
+        return False
+
+def return_word(text):
+    if(check_first_word(text)):
+        print(text + "is last word")
+        add_used_word(text)
+        return text + "するますたー"
+    elif True:
+        return "this is not good"
+        
+    
