9b2c43f195ca98e77f12a65345d4639bf8a6c0d5
nasubiman <nasubimanza@gmail.com>
2023-09-14 16:31:04 +0900

    SiritoriLibraryを修正

--- include/siritori_library.py
+++ include/siritori_library.py
@@ -1,15 +1,10 @@
 class SiritoriLibrary:
 
-    def __init__(self):
-        self.used_word_list = []
-        self.word_list = ["apple", "elephant", "tiger", "rabbit", "cat", "dog"]
-        self.current_word = None
-
     def add_used_word(self,word):
         self.used_word_list.append(word)
 
     def show_used_word(self):
-        words = ", ".join(self.word_list)
+        words = ", ".join(self.used_word_list)
         return words
 
     def check_first_word(self, word):
@@ -17,13 +12,23 @@ class SiritoriLibrary:
             return True
         else:
             return False
+    
+    def check_last_word(self, word):
+        if (not word[-1] in { "ん", "ン"}):
+            return True
+        else:
+            return False
 
     def return_word(self, text):
-        if(self.check_first_word(text) and not text in self.used_word_list):
+        if(self.check_first_word(text) and self.check_last_word(text) and not text in self.used_word_list):
             print(text + "is last word")
             self.add_used_word(text)
-            return text + "するますたー"
+            return text + "ますたー"
         elif True:
-            return "this is not good"
+            return "その言葉はだめです"
+        
+    def __init__(self):
+        self.used_word_list = []
+        self.word_list = ["アイヌ", "異次元", "餓えた", "英断", "落ちぶれた", "蚊取り線香"]
             
         
