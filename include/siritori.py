e84662485c4682cce2e2de2b1332afa4ed7dd2ea
nasubiman <nasubimanza@gmail.com>
2023-09-14 16:08:44 +0900

    最低限はできるようにした

--- include/siritori.py
+++ include/siritori.py
@@ -1,14 +1,18 @@
 from include import siritori_library
+
+siritori_lib = siritori_library.SiritoriLibrary()
 def siritori(text , flag):
+    global siritori_lib
     if(not flag):
-        siritori_library.add_used_word(text)
+        siritori_lib = siritori_library.SiritoriLibrary()
+        siritori_lib.add_used_word(text)
         return 'しりとりを始めるよ！ 終わりたかったら!owari と打ってね  まずは自分から\n しりとりますたー', True
 
     elif(text == '!owari'):
         return "しりとりを終了するよ！ またね！\n あなたのスコアは無しです", False
     
     elif(text == "!showlist"):
-        return siritori_library.show_used_word, True
+        return siritori_lib.show_used_word(), True
     
     elif True:
-        return siritori_library.return_word(text) , True
+        return siritori_lib.return_word(text) , True
--- include/siritori_library.py
+++ include/siritori_library.py
@@ -1,28 +1,29 @@
-used_word_list = []
+class SiritoriLibrary:
 
-word_list = ["apple", "elephant", "tiger", "rabbit", "cat", "dog"]
+    def __init__(self):
+        self.used_word_list = []
+        self.word_list = ["apple", "elephant", "tiger", "rabbit", "cat", "dog"]
+        self.current_word = None
 
-current_word = None
+    def add_used_word(self,word):
+        self.used_word_list.append(word)
 
-def add_used_word(word):
-    used_word_list.append(word)
+    def show_used_word(self):
+        words = ", ".join(self.word_list)
+        return words
 
-def show_used_word():
-    print(used_word_list)
-    return used_word_list
+    def check_first_word(self, word):
+        if (word[0] in { "た", "タ"}):
+            return True
+        else:
+            return False
 
-def check_first_word(word):
-    if(word[0] == "た" or word[0] == "タ"):
-        return True
-    else:
-        return False
-
-def return_word(text):
-    if(check_first_word(text)):
-        print(text + "is last word")
-        add_used_word(text)
-        return text + "するますたー"
-    elif True:
-        return "this is not good"
+    def return_word(self, text):
+        if(self.check_first_word(text) and not text in self.used_word_list):
+            print(text + "is last word")
+            self.add_used_word(text)
+            return text + "するますたー"
+        elif True:
+            return "this is not good"
+            
         
-    
